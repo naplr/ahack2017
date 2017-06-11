31016992554c1c077fca6fabfd5711587f45e4e9
Napol Rachatasumrit <naplr42@gmail.com>
2017-06-11 11:40:18 +0700

    Fix bugs

--- backend/apiapp/admin.py
+++ backend/apiapp/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register([Drop, Filter, ApiUser, UserDrop, FoundDrop])
+admin.site.register([Drop, Filter, ApiUser, UserDrop])
