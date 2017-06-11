946c2dd7ad7c71c7355d50292909a2f9d499c561
Napol Rachatasumrit <naplr42@gmail.com>
2017-06-11 11:16:44 +0700

    lkjlk

--- backend/apiapp/views.py
+++ backend/apiapp/views.py
@@ -149,7 +149,7 @@ def get_drops(request):
                 'id': x.id,
                 'name': x.name,
                 'image': x.image.url,
-                'creator': x.creator.name
+                'creator': x.creator.userId
             })
             
         return JsonResponse(list(res), safe=False)
